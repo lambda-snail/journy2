149ff338b6ad46eb5463fd60417bb5888d38338f
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-08-26 10:28:46 +0200

    Add linux path to main so that both win and linux are there, for the time being

--- source/main.cpp
+++ source/main.cpp
@@ -18,11 +18,8 @@ IMPLEMENT_APP(MyApp)
 
 bool MyApp::OnInit()
 {
-    //p_Db = std::make_unique<todo::DatabaseManager>(R"(C:\Projects\cpp\journy2\todo.db)");
-    p_Db = new todo::DatabaseManager(R"(C:\Projects\cpp\journy2\resources\todo.db)");
-
-//    todo::JournalEntry j( wxDateTime(17, wxDateTime::Month::Jan, 2023), "Hello World!" );
-//    p_Db->AddNewJournalEntry(j);
+    //p_Db = new todo::DatabaseManager(R"(C:\Projects\cpp\journy2\resources\todo.db)");
+    p_Db = new todo::DatabaseManager(R"(/home/niclas/projects/cpp/journy2/resources/todo.db)");
 
     wxInitAllImageHandlers();
     auto* frame = new JournyMainFrame(p_Db, nullptr, wxID_ANY, wxEmptyString);
