b24b6b8a0184494eee371225a42a51208d8e03bc
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-08-29 20:16:38 +0200

    Solve the problem of having to switch database path on windows and linux

--- source/main.cpp
+++ source/main.cpp
@@ -18,8 +18,12 @@ IMPLEMENT_APP(MyApp)
 
 bool MyApp::OnInit()
 {
-    //p_Db = new todo::DatabaseManager(R"(C:\Projects\cpp\journy2\resources\todo.db)");
+    // Temporary solution during initial development
+#ifdef WIN32
+    p_Db = new todo::DatabaseManager(R"(C:\Projects\cpp\journy2\resources\todo.db)");
+#else
     p_Db = new todo::DatabaseManager(R"(/home/niclas/projects/cpp/journy2/resources/todo.db)");
+#endif
 
     wxInitAllImageHandlers();
     auto* frame = new JournyMainFrame(p_Db, nullptr, wxID_ANY, wxEmptyString);
