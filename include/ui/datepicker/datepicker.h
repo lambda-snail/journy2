5166e4a233b32ae5fe9cc1aa07401eea0cb56efc
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-09-30 00:06:39 +0200

    add description to the date picker function

--- include/ui/datepicker/datepicker.h
+++ include/ui/datepicker/datepicker.h
@@ -18,5 +18,15 @@ namespace ImGuiExtensions
         Years
     };
 
+    /**
+     * Displays a date picker widget that allows for choosing a date. The date picker is must be embedded in a window or
+     * child widget, or in a modal. Returns true when the selection is done.
+     * @param id The id of the widget
+     * @param level The selection level - days, months or years. This is managed by the date picker but can also be used
+     *  to programmatically change the current stage of selection.
+     * @param t If DatePicker returns true, this will be set to the selected date. If DatePicker returns false this will
+     *  contain an intermediate stage of selection.
+     * @return true iff the selection is complete.
+     */
     bool DatePicker(char const* id, DatePickerLevel& level, std::chrono::year_month_day& t) noexcept;
 }
